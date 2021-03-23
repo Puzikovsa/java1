1306a42a94c54e8812a82063d74859ad26694a07
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-03-23 10:16:04 +0300

    22.03.21 v_1.2

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,4 +1,4 @@
-package ru.progwards.java1.lessons.compar_if_cycles;
+package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
     public static boolean containsDigit(int number, int digit) {
--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -1,4 +1,4 @@
-package ru.progwards.java1.lessons.compar_if_cycles;
+package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleSimpleInfo {
     public static int maxSide(int a, int b, int c){
