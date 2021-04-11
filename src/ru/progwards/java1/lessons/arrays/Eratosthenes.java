3d2d3b6b1c6cf4feadc065141ccdb3d8167111e2
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-04-11 11:48:57 +0300

    arrays v.11.04

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -1,4 +1,32 @@
 package ru.progwards.java1.lessons.arrays;
 
+import java.util.Arrays;
+
 public class Eratosthenes {
+    private boolean[] sieve;
+    private int N;
+
+    public Eratosthenes(int N){
+        sieve = new boolean[N];
+        this.N = N;
+        Arrays.fill(sieve, true);
+        sift();
+    }
+    private void sift() {
+        sieve[0] = false;
+        sieve[1] = false;
+                for (int i = 2; i < N - 1; i++) {
+                for (int j = i + i; j < N - 1; j+=i) {
+                    sieve[j] = false;
+                }
+        }
+    }
+    public boolean isSimple(int n){
+        return sieve[n];
+    }
+
+    public static void main(String[] args) {
+        Eratosthenes era = new Eratosthenes(20);
+        System.out.println(era.isSimple(19));
+    }
 }
