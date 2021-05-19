6ca8e8e772c90ba47430eea02e862fb7bd86a009
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-05-19 23:01:30 +0300

    interfaces 19.05

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -1,10 +1,11 @@
 package ru.progwards.java1.lessons.classes;
 
-public class Animal {
+import ru.progwards.java1.lessons.interfaces.FoodCompare;
+
+public class Animal<animal>{
     private double weight;
 
     public Animal(double weight){
-
         this.weight = weight;
     }
     public AnimalKind getKind(){
@@ -23,19 +24,66 @@ public class Animal {
     public double getFoodCoeff(){
         return 0.02;
     }
+    //вычисляем вес корма для животного в зависимости от его веса
     public double calculateFoodWeight(){
         return weight * getFoodCoeff();
     }
     public String toStringFull(){
         return "I am " + getKind() + ", eat " + getFoodKind() + " " + calculateFoodWeight();
     }
-
-    public static  void main(String[] args) {
-        Animal dog = new Animal(50.0);
-        System.out.println(dog.toString());
-        System.out.println(dog.toStringFull());
+    // сравнение животных по весу
+    @Override
+    public boolean equals(Object anObject){
+        if(this == anObject) return true;
+        if(anObject == null || getClass() != anObject.getClass()) return false;
+        Animal animal = (Animal) anObject;
+        if(this.getWeight() == ((Animal) anObject).getWeight()) return true;
+        else return false;
     }
 
+            //информация о цене 1 кг еды
+    public double getFood1kgPrice() {
+
+        switch (getFoodKind()) {
+            case HAY:
+                return 20.0;
+            case CORN:
+                return 50.0;
+            case UNKNOWN:
+                return 0.0;
+        }
+        return 0.0;
+    }
+
+        //возвращаем стоимость еды для животного в зависимости от его веса и типа
+        public double getFoodPrice() {
+            return calculateFoodWeight() * getFood1kgPrice();
+        }
+
+//    //сравнение цен на еду для разных животных
+//    public int compareFoodPrice(Animal aminal){
+//    Double.compare(aminal,Animal.this);
+//
+//    }
+
+            public static void main (String[]args){
+                Animal dog = new Animal(20.0);
+                Hamster bich = new Hamster(20.0);
+                Duck skruge = new Duck(20.0);
+                Animal cat = new Animal(20.0);
+                System.out.println(cat.toString());
+                System.out.println(dog.toStringFull());
+                System.out.println(AnimalKind.DUCK.equals(AnimalKind.HAMSTER));
+                System.out.println(cat.equals(dog));
+                System.out.println(bich.equals(skruge));
+                System.out.println(skruge.toStringFull());
+                System.out.println(skruge.getFoodPrice());
+            }
+        }
 
 
-}
+//    @Override
+//    public int compareFoodPrice() {
+//        return 0;
+//    }
+//}
