3e850d55d4a9a1472e6f13e7157432cccf0b333a
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-05-24 16:39:38 +0300

    interface 24.05 v.3

--- src/ru/progwards/java1/lessons/interfaces/Food.java
+++ src/ru/progwards/java1/lessons/interfaces/Food.java
@@ -1,5 +1,7 @@
 package ru.progwards.java1.lessons.interfaces;
 
+import java.util.Arrays;
+
 public class Food implements CompareWeight{
     private int weight;
 
@@ -17,10 +19,23 @@ public class Food implements CompareWeight{
         else if(getWeight() > ((Food) smthHasWeigt).getWeight()) return CompareResult.GREATER;
         else return CompareResult.EQUAL;
     }
+    public static void sort(CompareWeight[] a){
+//        for(int i = 1; i < a.length; i++){
+//            for(int j = 0; j < a.length; j++){
+//                int per = a[i];
+//                int per1 = a[j];
+//                if (per < per1){
+//                    a[i] = per1;
+//                    a[j] = per;
+//                }
+//            }
+        }
 
     public static void main(String[] args) {
         Food corn = new Food(100);
         Food proso = new Food(260);
-        System.out.println(corn.compareWeight(proso));
+        Food brad = new Food(150);
+        System.out.println(proso.compareWeight(brad));
+        int[] a = {6, 12, 26, 62, -6};
     }
 }
