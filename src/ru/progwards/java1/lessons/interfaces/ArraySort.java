e31fc2efe6a2ab239d522083e50fb70476509620
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-05-25 20:16:57 +0300

    interface 25.05 v.4

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -1,19 +1,45 @@
 package ru.progwards.java1.lessons.interfaces;
 
-public class ArraySort{
+import java.util.Arrays;
 
-    public static void sort(CompareWeight[] a) {
-//        for (int i = 1; i < a.length; i++) {
-//            for (int j = 0; j < a.length; j++) {
-//                int per = a[i];
-//                int per1 = a[j];
-//                if (per < per1) {
-//                    a[i] = per1;
-//                    a[j] = per;
-//                }
-//            }
-//        }
-//    }
+public class ArraySort {
+//    public ArraySort;
+
+    public static void sort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                int per = a[i];
+                int per1 = a[j];
+                if (per < per1) {
+                    a[i] = per1;
+                    a[j] = per;
+                }
+            }
+        }
     }
 
+    public static void sort(CompareWeight[] a) {
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                CompareWeight per = a[i];
+                CompareWeight per1 = a[j];
+                if (per.compareWeight(per1) == CompareWeight.CompareResult.LESS) {
+                    a[i] = per1;
+                    a[j] = per;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Animal bird = new Animal(12.0);
+        Hamster ivanich = new Hamster(0.4);
+        Duck skruge = new Duck(1.2);
+        Cow burenka = new Cow(150.2);
+        CompareWeight[] animals = {bird, ivanich, skruge, burenka};
+        System.out.println(animals.toString());
+        System.out.println(Arrays.stream(animals).sorted());
+        sort(animals);
+        System.out.println(Arrays.toString(animals));
+    }
 }
