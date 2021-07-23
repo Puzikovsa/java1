0b5fc466f4b6224caafcac37dbb036a990ad25ab
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-07-23 15:08:49 +0300

    lesson10_23.07_v.1

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -4,30 +4,33 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class CharFilter {
-    public static void filterFile(String inFileName, String outFileName, String filter) {
-        try {
-            FileReader reader = new FileReader(inFileName);
-            try (FileWriter writer = new FileWriter(outFileName)) {
-                for (int ch; (ch = reader.read()) >= 0; ) {
-                    int a = Integer.parseInt(filter);
-                    if (ch != a) {
-                        writer.write(ch);
-                    }
+    public static void filterFile(String inFileName, String outFileName, String filter) throws IOException {
+
+        String result = "";
+        FileReader reader = new FileReader(inFileName);
+        Scanner scanner = new Scanner(reader);
+        FileWriter writer = new FileWriter(outFileName);
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            for (int i = 0; i <= str.length() - 1; i++) {
+                if (filter.indexOf(str.charAt(i)) == -1) {
+                    result = result + str.charAt(i);
                 }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+                writer.write(result);
+            }
+            writer.close();
         }
-    }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        filterFile("C:\\Users\\puzik\\IdeaProjects\\Testing_lesson_10\\File.txt", "text2.txt","C:\\Users\\puzik\\IdeaProjects\\hello world\\obcsene.txt");
-        System.out.println("Программа закончила работу");
+
+    public static void main(String[] args) throws IOException {
+        String obcsene = " ./,";
+        filterFile("C:\\Users\\puzik\\IdeaProjects\\Testing_lesson_10\\File.txt", "text2.txt", obcsene);
+        System.out.println("Программа завершила работу");
     }
 }
+
