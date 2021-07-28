d606cc2a5bd1c452c4ea783ab063a72f8e6cf5fe
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-07-28 17:02:48 +0300

    lesson10_28.07_v.1

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -1,16 +1,14 @@
 package ru.progwards.java1.lessons.io1;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class CharFilter {
     public static void filterFile(String inFileName, String outFileName, String filter) throws IOException {
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         FileReader reader = new FileReader(inFileName);
         Scanner scanner = new Scanner(reader);
         FileWriter writer = new FileWriter(outFileName);
@@ -18,17 +16,17 @@ public class CharFilter {
             String str = scanner.nextLine();
             for (int i = 0; i <= str.length() - 1; i++) {
                 if (filter.indexOf(str.charAt(i)) == -1) {
-                    result = result + str.charAt(i);
+                    result.append(str.charAt(i));
                 }
             }
-                writer.write(result);
-            }
-            writer.close();
         }
+        writer.write(result.toString());
+        writer.close();
+    }
 
 
     public static void main(String[] args) throws IOException {
-        String obcsene = " ./,";
+        String obcsene = "ЭЖ ./,";
         filterFile("C:\\Users\\puzik\\IdeaProjects\\Testing_lesson_10\\File.txt", "text2.txt", obcsene);
         System.out.println("Программа завершила работу");
     }
---
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -0,0 +1,25 @@
+package ru.progwards.java1.lessons.io2;
+
+import java.util.Arrays;
+
+public class PhoneNumber {
+
+
+    public static String format(String phone) throws Exception {
+        String phoneNum;
+        StringBuilder resultNum = new StringBuilder();
+        for (char c : phone.toCharArray())
+            if (Character.isDigit(c))
+                resultNum.append(c);
+        if (resultNum.length() >= 10 & resultNum.length() <= 11) {
+            phoneNum = resultNum.toString();
+            phoneNum = "+7" + phoneNum.substring(1);
+        } else throw new Exception();
+
+        return phoneNum;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(format("+8(924) 265 45 - 54"));
+    }
+}
