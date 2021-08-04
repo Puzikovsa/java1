9d9bce4348dfb325e8b7506eec0191457041b4b9
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-08-04 19:50:54 +0300

    lesson11_4.08_v.3

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -8,42 +8,61 @@ import java.util.Scanner;
 
 public class Censor {
 
-    public static void censorFile(String inoutFileName, String[] obscene) throws FileNotFoundException {
-        FileReader reader = new FileReader(inoutFileName);
-        Scanner scan = new Scanner(reader);
-        while (scan.hasNextLine()) {
-            StringBuilder str = new StringBuilder(scan.nextLine());
-            System.out.println(str);
-            for (int i = 0; i < str.length(); i++) {
-                for (String s : obscene) {
-                    int charOn = str.indexOf(s);
-                    if (charOn != -1) {
-                        String str1 = s;
-                        for (int a = 0; a < str1.length(); a++) {
-                            str1 = str1.replace(str1.charAt(a), '*');
+    public static void censorFile(String inoutFileName, String[] obscene) throws CensorExeption {
+        try {
+            FileReader reader = new FileReader(inoutFileName);
+            Scanner scan = new Scanner(reader);
+            while (scan.hasNextLine()) {
+                StringBuilder str = new StringBuilder(scan.nextLine());
+                System.out.println(str);
+                for (int i = 0; i < str.length(); i++) {
+                    for (String s : obscene) {
+                        int charOn = str.indexOf(s);
+                        if (charOn != -1) {
+                            String str1 = s;
+                            for (int a = 0; a < str1.length(); a++) {
+                                str1 = str1.replace(str1.charAt(a), '*');
+                            }
+                            str.replace(charOn, (charOn + s.length()), str1);
                         }
-                        str.replace(charOn, (charOn + s.length()), str1);
                     }
-                }
-                }
-            System.out.println(str);
-            try {
-                FileWriter writer = new FileWriter(inoutFileName);
-                writer.write(str.toString());
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }finally {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+
                 }
             }
+            FileWriter writer = new FileWriter(inoutFileName);
+            System.out.println(str);
+            writer.write(str.toString());
+            writer.close();
+            reader.close();
         }
+        catch (IOException e) {
+            throw new CensorExeption(inoutFileName, e.getMessage());
+        }
+    }
+
+    public static class CensorExeption extends Exception {
+        String fileName;
+        String message;
+
+        CensorExeption(String fileName, String message){
+            this.fileName = fileName;
+            this.message = message;
         }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        censorFile("src/ru/progwards/java1/lessons/io2/testText.txt", new String[]{"Java", "Oracle", "Sun", "Microsystems"});
+
+        @Override
+        public String toString() {
+            return fileName + ":" + message;
+        }
+
+    }
+
+    public static void main(String[] args){
+        try {
+            censorFile("src/ru/progwards/java1/lessons/io2/testText.txt", new String[]{"Java", "Oracle", "Sun", "Microsystems"});
+        }
+        catch (CensorExeption e){
+            System.out.println(e.toString());
+        }
     }
 }
