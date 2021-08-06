ce730051d363be1c94dfdb457aeb7dca8fc48a61
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-08-06 22:16:06 +0300

    lessons12_06.08.21_v.3

--- src/ru/progwards/java1/lessons/collections/Creator.java
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -2,7 +2,6 @@ package ru.progwards.java1.lessons.collections;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 
 public class Creator {
 
@@ -13,6 +12,7 @@ public class Creator {
         }
         return arrayList1;
     }
+
     public static Collection<Integer> fillOdd(int n){
         ArrayList<Integer> arrayList2 = new ArrayList<>(n);
         for (int i = n - 1; i >= 0; i--){
@@ -21,8 +21,21 @@ public class Creator {
         return arrayList2;
     }
 
+    public static ArrayList<Integer> fill3(int n){
+        ArrayList<Integer> arrayList3 = new ArrayList<>(n*3);
+        for (int i = 0; i < n*3; i++){
+            String a = String.valueOf(i);
+            String b = String.valueOf(i * i);
+            String c = String.valueOf(i * i * i);
+            String num = a + b + c;
+                int result = Integer.parseInt(num);
+                arrayList3.add(result);
+        }
+        return arrayList3;
+    }
+
     public static void main(String[] args) {
-        for (Integer i : fillOdd(5)) {
+        for (Integer i : fill3(7)) {
             System.out.println(i);
         }
     }
