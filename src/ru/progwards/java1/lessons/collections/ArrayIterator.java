c187efa0266c62b4a6280f8c841307451dd48ef2
Puzikovsa <80515484+Puzikovsa@users.noreply.github.com>
2021-08-10 16:29:56 +0300

    lessons12_10.08.21_v.2

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -1,21 +1,42 @@
 package ru.progwards.java1.lessons.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class ArrayIterator<T> implements Iterator<T> {
 
-    private  T[] array;
+    private T[] array;
 
-    ArrayIterator(T[] array){
+    ArrayIterator(T[] array) {
         this.array = array;
     }
+
     @Override
     public boolean hasNext() {
-        return false;
+        return true;
     }
 
     @Override
     public T next() {
-        return null;
+        Object T = null;
+        for (int i = 0; i < array.length; i++) {
+            T = array[i + 1];
+        }
+        return (T) T;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int i = 0; i < 20; i++) {
+            arrayList.add(i + 1);
+        }
+        Object[] array1 = arrayList.toArray();
+        System.out.println(Arrays.toString(array1));
+        System.out.println(array1[5]);
+        System.out.println(new ArrayList<>(Collections.singleton(new ArrayIterator[5])));
+
     }
 }
+
